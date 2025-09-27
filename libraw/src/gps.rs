f978560021a642285fdc97bf39accbfd6de1fe72
hexilee <i@hexilee.me>
2025-09-27 15:41:10 +0800

    startup
    Signed-off-by: hexilee <i@hexilee.me>

---
+++ libraw/src/gps.rs
@@ -0,0 +1,20 @@
+use libraw_sys as sys;
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct GpsInfo {
+    pub latitude: [f32; 3],
+    pub longitude: [f32; 3],
+    pub gpstimestamp: [f32; 3],
+    pub altitude: f32,
+}
+
+impl From<sys::libraw_gps_info_t> for GpsInfo {
+    fn from(data: sys::libraw_gps_info_t) -> Self {
+        Self {
+            latitude: data.latitude,
+            longitude: data.longitude,
+            gpstimestamp: data.gpstimestamp,
+            altitude: data.altitude,
+        }
+    }
+}
